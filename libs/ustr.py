f274a22b06de3eedf76e8ded531d73678e5f8072
Bharath B <bharath@instore.ai>
2018-08-05 23:29:37 +0530

    Fix python2 + Qt5 string decoding

--- libs/ustr.py
+++ libs/ustr.py
@@ -4,11 +4,14 @@ def ustr(x):
     '''py2/py3 unicode helper'''
 
     if sys.version_info < (3, 0, 0):
-        from PyQt4.QtCore import QString
-        if type(x) == str:
-            return x.decode('utf-8')
-        if type(x) == QString:
+        try:
+            from PyQt4.QtCore import QString
+            if type(x) == str:
+                return x.decode('utf-8')
+            if type(x) == QString:
+                return unicode(x)
+        except:
+            # No Qt4. Qt5 automatically converts QString to string
             return unicode(x)
-        return x
     else:
         return x  # py3
